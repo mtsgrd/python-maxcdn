cb72bb98489d7d205c5134f286d744c33a739600
Mattias Granlund <mtsgrd@gmail.com>
2014-05-13 15:25:28 +0200

    Typo

--- maxcdn/maxcdn.py
+++ maxcdn/maxcdn.py
@@ -7,13 +7,25 @@ try:
 except ImportError: # handly python 3.x
     from urllib import parse as urlparse
 
+
+"""This is a python API module for MaxCDN services."""
+
 class MaxCDN(object):
+    """A class for interacting with the MaxCDN API.
+
+    Reade more at https://github.com/maxcdn/api-docs
+    """
 
     logger = logging.getLogger(__name__)
+    max_retries = 5
 
     def __init__(self, alias, key, secret, server="rws.maxcdn.com", **kwargs):
         self.url    = "https://%s/%s" % (server, alias)
         self.client = OAuth1(key, client_secret=secret, **kwargs)
+
+
+    def _make_request(self, method, *args, **kwargs):
+        while retries > 0:
 
     def _get_headers(self, json=True):
         headers = { "User-Agent": "Python MaxCDN API Client" }
@@ -33,17 +45,15 @@ class MaxCDN(object):
             if type(params) is str:
                 params = urlparse.parse_qs(params)
             data = params
-
-        self.logger.debug('Requesting %s', self_get_url(end_point))
-        return getattr(self.client, method)(self._get_url(end_point),
-                data=data, headers=self._get_headers(json=True),
-                **kwargs)
+        self.logger.debug('Requesting %s', self._get_url(end_point))
+        method = getattr(self.client, method) 
+        return self._make_request(method, self._get_url(end_point), data=data,
+                headers=self._get_headers(json=True), **kwargs)
 
     def get(self, end_point, **kwargs):
         self.logger.debug('Requesting %s', self._get_url(end_point))
         return self.client.get(self._get_url(end_point),
-                headers=self._get_headers(json=False),
-                **kwargs)
+                headers=self._get_headers(json=False), **kwargs)
 
     def patch(self, end_point, data=None, **kwargs):
         return self._data_request("post", end_point, data=data, **kwargs)
@@ -62,5 +72,6 @@ class MaxCDN(object):
         if file_or_files is not None:
             return self.delete(path, data = { "files": file_or_files },
                     **kwargs)
+        self.logger.debug('Purging %s from %s', zoneid, file_or_files)
         return self.delete(path, **kwargs)
 
